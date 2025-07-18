a9b50de1ad25edde3d0a4e142b8330f482e8d40b
DeusWyvern <dpefley98@gmail.com>
2025-07-18 10:15:26 -0700

    Added LSPs to init to sync between computers.

--- init.lua
+++ init.lua
@@ -674,6 +674,12 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         pyright = {},
+        cssls = {},
+        html = {},
+        htmlhint = {},
+        quick_lint_js = {},
+        ruff = {},
+        shfmt = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
